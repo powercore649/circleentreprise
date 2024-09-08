a04a1cdd2224aade24f78c3a60039b54e814aeff
powercore649 <126120353+powercore649@users.noreply.github.com>
2024-09-08 14:43:03 -0400

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -4,6 +4,9 @@ const UnkownPage = () => {
         'Have you lost your way, kiddo?',
         'Damn it! This dead-end road.',
         'The Aurors blocked this road, Harry!'
+        'oops, circle protect is lost :('
+        'oops, circle protect is lost :('
+        'https://discord.gg/empire-auroras-1235157719409885184'
     ];
 
     return <ErrorPage code={404} message={messages[Math.floor(Math.random()*messages.length)] || "Page not found."} />
