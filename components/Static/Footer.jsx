098d4339de87e54a7054db84abd23e5cd5999ccd
powercore649 <126120353+powercore649@users.noreply.github.com>
2024-09-08 15:18:22 -0400

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -54,7 +54,7 @@ export default function Footer() {
                             <div>
                                 <Link href="https://discord.gg/empire-auroras-1235157719409885184">
                                     <a className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                    <i className={`fa-brands fa-discord`} /> Disocrd 
+                                    <i className={`fa-brands fa-discord`} /> Discord 
                                     </a>
                                 </Link>
                             </div>
