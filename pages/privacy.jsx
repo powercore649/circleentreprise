7615e975a3fe572311c5ac51729825d84d953a24
powercore649 <126120353+powercore649@users.noreply.github.com>
2024-09-08 01:25:29 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/privacy.jsx
@@ -0,0 +1,20 @@
+export default function Privacy() {
+
+    return (
+        <div>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for circle protect</h1>
+            <p className="text-sm text-white/30 mb-4">Last Update: 2024-09-07  🆕️</p>
+  <div className="animateHeader text-white text-opacity-40 mt-5">
+        
+        <p>⊳ Every user can use thecircle protect bot however they want, as long as they do not intend to harm.</p>
+        <div className="py-3"></div>
+        <p>⊳ You cannot use certain commands at the same time while using MusicMaker.</p>   
+        <div className="py-3"></div>
+        <p>⊳ circle protect bot does not save any data based on users.</p>
+        <div className="py-3"></div>
+        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+    
+        </div>
+            </div>
+    );
+};
